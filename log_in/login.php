10a05388a3708f9649e1512d79b3ad4d7bd87ef1
Mahesh Madushan <madu140@gmail.com>
2020-03-21 13:36:30 +0530

    Initial commit

---
+++ log_in/login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style/style.css">
+    <title>Login</title>
+</head>
+
+<body>
+
+    <div class="bg-image">
+        <?php include './header.php' ?>
+        <div class="form-input">
+            <div class="div-relative">
+                <form action="#" method="post">
+                    <input class="texbox-styles" type="text" id="name" name="name" placeholder="Username">
+                    <br>
+                    <input class="texbox-styles" type="password" name="password" id="password" placeholder="Password">
+                    <br>
+                    <input type="submit" value="Login" id="submit">
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+
+</body>
+
+</html>
